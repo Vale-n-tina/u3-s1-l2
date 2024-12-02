0c3cd922d01f884e0d59824b8a46953aaf1de151
Vale-n-tina <valentia.grg@gmail.com>
2024-12-02 13:56:55 +0100

    .

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,14 +1,17 @@
 import { Component } from "react";
-import { Card, Col, Button } from "react-bootstrap";
+import { Card, Col, Button,Modal } from "react-bootstrap";
 import CommentArea from "./CommentArea";
+import AddComment from "./AddComment";
 
 class SingleBook extends Component {
   state = {
     selected: false,
+    modal:false
   };
 
   render() {
     return (
+        <>
       <Col xs={6} md={4} lg={2}>
         <Card
           className={this.state.selected ? "selected" : ""}
@@ -29,8 +32,21 @@ class SingleBook extends Component {
           </Card.Body>
         </Card>
         {this.state.selected && <CommentArea id={this.props.Book.asin} />}
-        {this.state.selected && <Button variant="primary">Add comment</Button>}
+        {this.state.selected && <Button onClick={()=>{this.setState({modal:true})}} variant="primary">Add comment</Button>}
       </Col>
+      <Modal show={this.state.modal} onHide={()=>{this.setState({modal:false})}}>
+      <Modal.Header closeButton>
+        <Modal.Title>Wtìrite your feedback</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>  <AddComment/> </Modal.Body>
+      <Modal.Footer>
+       
+        <Button variant="primary" >
+          Save Feedback
+        </Button>
+      </Modal.Footer>
+    </Modal>
+    </>
     );
   }
 }
